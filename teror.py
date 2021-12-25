ec9d99a24cc066bececf67827f62d3e9bc117f0e
Ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2021-12-26 00:30:39 +0330

    Add files via upload

---
+++ teror.py
@@ -0,0 +1,198 @@
+# MEHRAB TEROR☘️ 
+.
+#this is bug !                                            # slm                                                           #import library's
+print("Loading terors")
+import os
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{RED} ")
+
+print (Fore.DARK + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{YELLOW} ")
+print ("         Script By")
+print (Fore.VIOLET + "")
+x = """ MEHRAB TEROR  bot running....
+.                       
+               ---------------------------------------
+                -------------------------------------------
+              ____________MEHRAB TEROR_________________
+             _________________________________________
+            ———————————————————————————————————————————————————
+           ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+          ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠                    ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+        ××××××××______×××××××××××××××××××××××××××××××××_____×××××××                ‡‡†‡‡‡‡‡‡|     |†‡†‡‡†‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡††††††
+       ‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡††‡‡‡‡‡              ‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡_____‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡_____‡‡‡†‡‡‡‡‡              ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡              ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\\\\\\\\\‡//////////////‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\                    MEHRAB TEROR    /‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡              ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\                       /‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+        ††††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+ 
+  TEROR      ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+          ________________________________________________
+           -------------------------------------------------------
+            +++++++++++++++++++++++++++++++++++++++++++++++++++++
+             *******************************************
+              ################################################
+
+MEHRAB TEROR
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+Making day:1400/10/4
+                            ⁶6⁶
+          ▄▀▄     ▄▀▄
+         ▄█░░▀▀▀▀▀░░█▄
+     ▄▄  █░░░░░░░░░░░█  ▄▄
+    █▄▄█ █░░█░░┬░░█░░█ █▄▄█
+                     !!!! Script is free !!!!
+                   created by Mehrab Teror☘️
+          //////id rubik' #new ~> @Hunter_bug
+//////id tel' ~> @My_serverr
+ zadam//////
+Filtering RUBIKA running //////////////////////////////////////////////////////////////////////////.......
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# My Mehrab*_*
+#my love malake Teror❤️💜♥️💜♥️💜♥️💜♥️💜♥️💜♥️💜♥️♥️💜♥️♥️♥️💜♥️💜♥️💜💜💜💜♥️♥️♥️💜♥️♥️
+#          ▄▀▄     ▄▀▄
+#         ▄█░░▀▀▀▀▀░░█▄
+#     ▄▄  █░░░░░░░░░░░█  ▄▄
+#    █▄▄█ █░░█░░┬░░█░░█ █▄▄█
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("ok welcome friends . 💙 . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA @My_serverr")
+print ("                         ID telegram @My_serverr")
+print ("                          New Id rubika : @Hunter_bug")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#_________")
+time.sleep(0.6)
+print ("##________")
+time.sleep(0.5)
+print ("###_______")
+time.sleep(0.4)
+print ("####______")
+time.sleep(0.3)
+print ("#####_____")
+time.sleep(0.2)
+print ("######____")
+time.sleep(0.1)
+print ("#######___")
+time.sleep(0.5)
+print ("########__")
+time.sleep(0.1)
+print ("#########_")
+time.sleep(0.5)
+print ("##########")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input(" 2 Code channel & 3 for group & 4 for ac > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print("""update =
+$       cd $HOME
+$       rm -rf Filtering-Rubika-mehrab
+$       git clone https://gitHub.com//mehrabterror.filtering-rubika
+""")
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '(https://Dxprit.moldyn.hacksors.lnpker.bip.http//(reaprot.(81.23.40.7)_filter.rest(+bit.ly/3fX8ljZhttps+×CFh/) (fg('green'), bg('Blue'), attr('reset')))
+
+
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s (http://fata.hack/g.w.p.h.o.s.l)/5.7.8.0.3.5.8.9.4.2.0.7/f/i///////h.207.497.U/p_(++c/0*.1.8.3.5.xxsx+×F)%s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("error - lotfan dorost vared konid")
+
+
+if up == "4":
+
+        print ('code for account ≠>' + '%s%s (https://rubika.ir/filtering.server/(hackes*fata.server/)https://video.sex.4.9.3.3.2.4.2.0.2.4.1.2.4.3.5.7.0//) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("error - lotfan dorost vared konid")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubikam =
+@Hi_room
+ """)
+print ("""
+link group rubika my love me ==>>>https://rubika.ir/joing/BHBEIDFC0NBLFQANCEKMCMJGJBQOVAKR""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Love you My Friend:delsa :)💜")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Baraye Inke Yeki Ro Filter Konid In Ro Befrestin' + '%s%s  / / %s' % (fg('red'), bg('Blue'), attr('reset')))
+print (f"{yellow} ")
+time.sleep(1)
+print ("""va belak konin va code ro ( 3 ) bar bezanin to gozaresha
+baraye account""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd $HOME")
+os.system("rm -rf /data/data/com.termux/files/home/Filtering-Rubika && git clone https://github.com/mehrabterror.filtering-rubika")
+print ("time end :")
+print (Fore.WHITE + "")
+date()
