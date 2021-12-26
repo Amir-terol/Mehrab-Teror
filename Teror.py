f7667615dff786d396c331abb5a01520d4474c34
Ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2021-12-26 23:40:50 +0330

    Add files via upload

---
+++ Teror.py
@@ -0,0 +1,207 @@
+import os
+print (" * M3HR4B T3R0R * ")
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.BLUE + "")
+x = """ Mehrab
+
+
+
+
+Hello My Friend Im Mehrab Teror I am filtering and I want to give you, my friends, a filtering code. I hope you use it correctly. Thank you.
+
+GOD FIltering Rubika >~Mehrab Teror>
+                           ▄▄▄  ▄▄▄                               ███  ███
+                           ████████
+                           ██ ██ ██
+                           ██ ▀▀ ██
+                           ██    ██                               ▀▀    ▀▀
+
+ │                                                                    ███████╗                            
+│                             █
+                               █╔════╝                              │
+│                             █████╗                               │
+│                             ██╔══╝                               │
+│                             ███████╗                             │
+│                              ══════╝
+│       
+
+                               ██╗  ██╗                             │
+│                              ██║  ██║                             │
+│                              ███████║                             │
+│                              ██╔══██║                             │
+│                              ██║  ██║                             │
+│                              ╚═╝  ╚═╝                             │
+
+
+
+│                              ██████╗                              │
+│                              ██╔══██╗                             │
+│                              ██████╔╝                             │
+│                              ██╔══██╗                             │
+│                              ██║  ██║                             │
+│                              ╚═╝  ╚═╝                 
+
+││                                                                    
+                               ██╔══██╗                             │
+│                              ███████║                             │
+│                              ██╔══██║                             │
+│                              ██║  ██║                             │
+│                              ╚═╝  ╚═╝                             │
+
+│                             
+                               ██████╗                              │
+│                              ██╔══██╗                             │
+│                              ██████╔╝                             │
+│                              ██╔══██╗                             │
+│                              ██████╔╝                             │
+│                              ╚═════╝                              │
+                     new~acont~rubika:@Hunter_bug
+                       id tel:@My_serverr
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+print ("")
+print(" US ")
+print("")
+print("")
+print("")
+print (" ")
+up = input(" Acc 1 + Chanel 2 + Gp 3 ~ >> " )
+print(f"{blue} ")
+
+
+
+if up == "1":
+
+        print ('Account  == ' + '%s%s (https://.lnpker.dlit.http//(reaprot.(81.23.40.7)_senotor.pro(+bit.ly/9ploftakhttps+×CFh) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up != "1":
+        print("    мy code ")
+
+
+if up == "2":
+        print (' Chanel Cod  == ' + '%s%s (http://3.py/F./h.k.p////6.4.(157.621/https://s4.uupload.ir/files/n5_j0e2.jpg)(.(/U/p.[157.6)l////8.9.0.2.4.7.9.3.1.5.2.7.8.3.0.1.0) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "2":
+    print("    My Cods ")
+
+if up == "3":
+
+        print (' GP Cod ==' + '%s%https://xexsx.py.5.3.6.3.6.6.4.7.4.7.4.7.8.8..7.8.5.7.8.5.8.5.8.8.5.(8.7.9.3.2.1)(.lnykt18)/(4.0.3.4/F/h.157.621/3749.93) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+
+if up != "3":
+        print("  мy code ")
+        
+        
+time.sleep(1)
+print(f"{pink} ")
+time.sleep(0.3)
+print (" Cod Ro 10 bar Bezanin Lotfa Va Dorost Cod Ro Bezanin Ta Fake Nasheh ")
+print(" ")
+print (" ")
+print (" Tank You ")
+time.sleep(0.6)
+print(f"{green}" )
+d = """
+
+Im Mehrab Teror
+
+Id Rubika: @Hunter_bug
+
+Chanel Rubika: @Hi_room
+
+Id Telegram : @My_Serverr
+
+
+Link Gap : https://rubika.ir/joing/BHBEIDFC0NBLFQANCEKMCMJGJBQOVAKR
+
+Ye Ostoreh
+"""
+
+time.sleep(4.3)
+print(f"{green} ")
+print (" Bye Bye Majazi  ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                    I Love You Malake Teror!mn 💜")
+print ("                    Godrat Majazi Mnm")
+print ("                    Khalegh Teror💙 ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("")
+time.sleep(0.7)
+print ("   Error")
+time.sleep(0.6)
+print ("   The End")
+time.sleep(0.5)
+print ("   Bye Bye ")
+time.sleep(0.4)
+print ("   God Filtering")
+time.sleep(0.3)
+print ("   Megrab Teror")
+time.sleep(0.2)
+print ("   Tank You")
+time.sleep(0.1)
+print ("   I Love")
+time.sleep(0.5)
+print ("   Mehrab")
+time.sleep(0.1)
+print ("   Bye")
+print ("")
+time.sleep(1)
+print(f"{red} ")
+time.sleep(0.2)
+print (""" !!! Update Script !!!
+
+
+rm -rf Mehrab Teror
+
+git clone https://github.com/Filtering-Rubika-Ryson/Mehrab-Teror
+
+cd Mehrab Teror
+
+chmod 777 *
+
+python Teror.py
+
+""")
